fc38150fe9ce381ed2a4e064298e825ef4ad72c8
AFreul kaium Joy <afreuljoy@gmail.com>
2022-01-12 17:57:07 +0600

    search bar prb

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,10 +1,21 @@
+import { Button } from "react-bootstrap";
 import React from 'react';
+import { FormControl, InputGroup } from 'react-bootstrap';
+import './banner.css'
 
 const Banner = () => {
     return (
-        <div>
-         <h2>Banner</h2>   
-        </div>
+        <div className='banner'>
+       <h2>Banner</h2> 
+      
+      <form action="#">
+      <input className='rounded-pill' /> 
+      <Button className='rounded-pill' variant="danger">Search</Button>
+      </form>
+        
+      
+   
+      </div>
     );
 };
 
