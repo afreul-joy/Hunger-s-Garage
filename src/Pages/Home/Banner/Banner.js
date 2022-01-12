7a580539fa1989e4e34953f8255ba1019b42a42f
AFreul kaium Joy <afreuljoy@gmail.com>
2022-01-13 01:11:28 +0600

    banner done

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,19 +1,19 @@
-import { Button } from "react-bootstrap";
 import React from 'react';
-import { FormControl, InputGroup } from 'react-bootstrap';
 import './banner.css'
 
 const Banner = () => {
     return (
-        <div className='banner d-flex justify-content-center align-items-center'>
-            <div className="">
+        <div className='banner'>
+            <div className="banner-text">
             <h1>Best Food Waiting For Your Belly</h1> 
-            <div class="input-group mx-auto container">
-            <input type="text" class="form-control" placeholder="Search Food Items" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+            </div>
+
+            <div class="input-group vertical-center container">
+            <input type="text" class="form-control " placeholder="Search Food Items" aria-describedby="button-addon2"/>
             <button class="btn btn-danger" type="button" id="button-addon2">Search</button>
             </div>
 
-            </div>
+            
 
       </div>
     );
