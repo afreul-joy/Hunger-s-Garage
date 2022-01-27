00a9d1c18a2ebcec9c07b16f907868d0ffd435f4
AFreul kaium Joy <afreuljoy@gmail.com>
2022-01-27 14:54:09 +0600

    form added

--- src/Pages/Meals/BuyNow/BuyNow.js
+++ src/Pages/Meals/BuyNow/BuyNow.js
@@ -1,12 +1,16 @@
+import { Button } from '@mui/material';
 import React, { useEffect, useState } from 'react';
-import { Card, Container,Row } from 'react-bootstrap';
+import { Card, Container,Form,Row } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
+import useAuth from '../../../hooks/useAuth';
 
 const BuyNow = () => {
     const [meal,setMeal] = useState({})
 
     const {id} = useParams()
 
+    const {user}=useAuth() 
+    //showing single details
     useEffect(() => {
         const url = `http://localhost:5000/meals/${id}`
         fetch(url)
@@ -14,9 +18,44 @@ const BuyNow = () => {
           .then(data => setMeal(data));
     },[])
 
+
+    //form method
+    const [phone,setPhone] = useState("")
+    const [address,setAddress] = useState("")
+
+    const handleNumber =e=>{
+        const phoneNumber = e.target.value;
+        setPhone(phoneNumber);
+    }
+    const handleAdress =e=>{
+        const Address = e.target.value;
+        setAddress(Address)
+    }
+
+    const handleForm=e=>{
+        e.preventDefault();
+        const userData = {name:user.displayName,email:user.email,productName:meal.name,productPrice:meal.price,phone:phone,address:address}
+        console.log(userData);
+
+        // Send data server POST API
+        fetch('https://crudserver01.herokuapp.com/users',{
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(userData)
+
+        })
+            .then(res=>res.json())
+            .then(data=>{
+                if(data.insertedId)
+                alert("Add Successfully")
+                e.target.reset();
+            })
+    }
+
+
     return (
         <div>
-            <h2>Lets buy</h2>
+            <h2 className="text-success my-2">Please Order </h2>
             <Container>
             <Row>
                 <div className="col-lg-6">
@@ -27,7 +66,19 @@ const BuyNow = () => {
                     </Card>
                 </div>
                 <div className="col-lg-6">
-                            <h2>Hello</h2>
+                    <h4 className="text-info mb-3">Order Requirement</h4>
+                    <div className="d-flex justify-content-center align-items-center">
+                        <Form  onSubmit={handleForm} className="w-50">
+                            <Form.Control type="text"  readOnly className="text-muted p-2  text-center" value={`Name: ${user.displayName}`  || ''} /> <br />
+                            <Form.Control type="text" readOnly className="text-muted p-2  text-center" value={`Email: ${user.email}` || ''} /> <br />
+                            <Form.Control type="text" readOnly className="text-muted p-2  text-center" value={`Meal Name: ${meal.name}` || ''} /> <br />
+                            <Form.Control type="text" readOnly className="text-muted p-2  text-center" value={`Price: ${meal.price}`|| ''} /> <br />
+                            <Form.Control type="text" required onChange={handleNumber} className="text-muted p-2  text-center" placeholder="Enter Your Phone Number" /> <br />
+                            <Form.Control type="text" required onChange={handleAdress}  className="text-muted p-2  text-center" placeholder="Enter Your Address" /> <br />
+                            <Button type="submit" variant="contained" className="mb-2" >Confirm</Button>
+                        </Form>
+                    </div>
+
                 </div>
                  </Row>
             </Container>
