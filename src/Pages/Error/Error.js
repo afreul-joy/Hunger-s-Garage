990db12437798d24dbf5c23ff10ba1e894af1884
AFreul kaium Joy <afreuljoy@gmail.com>
2022-02-14 00:22:26 +0600

    joy update

--- src/Pages/Error/Error.js
+++ src/Pages/Error/Error.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Error = () => {
-    return (
-        <div>
-           <h2>Page Not Found</h2> 
-        </div>
-    );
+  return (
+    <div>
+      <h2>Page Not Found</h2>
+    </div>
+  );
 };
 
 export default Error;
