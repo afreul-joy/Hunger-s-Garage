88f215f4086de379e750168fdf08c58077c0186b
AFreul kaium Joy <afreuljoy@gmail.com>
2022-08-14 21:51:23 +0600

    pagination comming

--- src/Pages/Meals/Meals.js
+++ src/Pages/Meals/Meals.js
@@ -1,14 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { Container, Row, Spinner } from "react-bootstrap";
 import ProductCard from "../Shared/ProductCard/ProductCard";
+import './Meals.css'
 
 const Meals = () => {
   const [foods, setFoods] = useState([]);
+  const [pageCount,setPageCount] = useState(0);
+  const [page,setPage] = useState(0)
+
   useEffect(() => {
-    const url = `https://hungers-garage.herokuapp.com/meals`;
+    const url = `http://localhost:5000/meals`;
     fetch(url)
       .then((res) => res.json())
-      .then((data) => setFoods(data));
+      .then((data) => { 
+        setFoods(data.product)
+        const count = data.count;
+        const pageNumber = Math.ceil(count /3);
+        setPageCount(pageNumber);
+      });
   }, []);
 
   return (
@@ -24,11 +33,28 @@ const Meals = () => {
             </Spinner>
           </div>
         ) : (
-          <Row xs={1} sm={1} md={2} lg={3} className="g-4">
+          <> 
+                    <Row xs={1} sm={1} md={2} lg={3} className="g-4">
             {foods?.map((food) => (
               <ProductCard key={food._id} food={food} />
             ))}
+
           </Row>
+
+          <div className="pagination">
+             {
+               [...Array(pageCount).keys()]
+               .map((number) =><button
+                  className={number===page?'selected':''}
+                    key={number}
+                    onClick={()=>setPage(number)}
+               >{number}</button>)
+             }
+            </div>
+
+          </>
+
+          
         )}
       </Container>
     </div>
